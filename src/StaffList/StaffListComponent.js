822aa0c8fb052264c45f4003497c68948932ede0
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-04-10 23:19:50 +0700

    6th

--- src/StaffList/StaffListComponent.js
+++ src/StaffList/StaffListComponent.js
@@ -1,26 +1,30 @@
 import { STAFFS, DEPARTMENTS } from './staffs';
 
 function Staff() {
-    const handleInfo = () => {
-        console.log(123);
-    }
-
-    const departments = DEPARTMENTS
+    var show = true;
 
     return(
         <div className='container'>
             <div className='row g-2'>
                 {STAFFS.map(staffinfo => (
-                    <div className="col-6" key={staffinfo.id}>
+                    <div className="col-lg-6 col-md-12" key={staffinfo.id}>
                         <div className="p-3 border bg-light accordion" id="accordionExample">
-                            <div className="accordion-item">
+                            <div className="accordion-item" onClick={() => {
+                                const setShow = document.getElementById(staffinfo.id)
+                                console.log(setShow)
+                                if (setShow.style.display == 'block') {
+                                    document.getElementById(staffinfo.id).style.display = 'none';
+                                } else {
+                                    document.getElementById(staffinfo.id).style.display = 'block';
+                                }
+                            }}>
                                 <h2 className="accordion-header" id="headingOne">
                                     <button className="accordion-button" type="button">
                                         <p>{staffinfo.name}</p>
                                     </button>
                                 </h2>
                                 <div>
-                                    <div className="staffinfo">
+                                    <div className="staffinfo" id={staffinfo.id}>
                                         <p><strong>Ngày sinh: </strong>{staffinfo.doB}</p>
                                         <p><strong>Ngày vào công ty: </strong>{staffinfo.startDate}</p>
                                         <p><strong>Phòng ban: </strong>{staffinfo.department.name}</p>
