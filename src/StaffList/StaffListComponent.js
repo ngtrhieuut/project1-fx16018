1fbc5fcf07c245adc1d2210b976815dff1c7f1fe
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-04-10 22:44:23 +0700

    5th

--- src/StaffList/StaffListComponent.js
+++ src/StaffList/StaffListComponent.js
@@ -1,17 +1,36 @@
 import { STAFFS, DEPARTMENTS } from './staffs';
 
 function Staff() {
+    const handleInfo = () => {
+        console.log(123);
+    }
+
+    const departments = DEPARTMENTS
 
     return(
         <div className='container'>
             <div className='row g-2'>
                 {STAFFS.map(staffinfo => (
-                    <div class="col-6">
-                        <div class="p-3 border bg-light">
-                            <p key={staffinfo.id}>{staffinfo.name}</p>
+                    <div className="col-6" key={staffinfo.id}>
+                        <div className="p-3 border bg-light accordion" id="accordionExample">
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="headingOne">
+                                    <button className="accordion-button" type="button">
+                                        <p>{staffinfo.name}</p>
+                                    </button>
+                                </h2>
+                                <div>
+                                    <div className="staffinfo">
+                                        <p><strong>Ngày sinh: </strong>{staffinfo.doB}</p>
+                                        <p><strong>Ngày vào công ty: </strong>{staffinfo.startDate}</p>
+                                        <p><strong>Phòng ban: </strong>{staffinfo.department.name}</p>
+                                        <p><strong>Số ngày nghỉ còn lại: </strong>{staffinfo.annualLeave}</p>
+                                        <p><strong>Số ngày đã làm thêm: </strong>{staffinfo.overTime}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    
                 ))}
             </div>
         </div>
@@ -19,3 +38,5 @@ function Staff() {
 }
 
 export default Staff;
+
+
